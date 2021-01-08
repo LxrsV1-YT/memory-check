66af74996b760ab04630ef12ff14eb1bd0b25e59
LxrsV1-YT <69675999+LxrsV1-YT@users.noreply.github.com>
2021-01-08 21:57:36 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+exports.memory = function(){
+    const arr = [1, 2, 3, 4, 5, 6, 9, 7, 8, 9, 10];
+    arr.reverse();
+    const used = process.memoryUsage().heapUsed / 1024 / 1024;
+    var mb = Math.round(used * 100) / 100
+    console.log(`${mb} MB in use!`);
+}
+// Please make sure to read
+// README.md
