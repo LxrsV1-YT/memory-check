920c1ed21e5df1c0ee78f6dbe73da493d1606c21
LxrsV1-YT <69675999+LxrsV1-YT@users.noreply.github.com>
2021-01-09 12:02:17 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-function memory() {
-    const arr = [1, 2, 3, 4, 5, 6, 9, 7, 8, 9, 10];
-    arr.reverse();
-    const used = process.memoryUsage().heapUsed / 1024 / 1024;
-    var mb = Math.round(used * 100) / 100
-    return mb;
-}
-
-module.exports.memory = this.memory;
