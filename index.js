1ad0abd58f6922a361f1ec7870881bad56ae194c
LxrsV1-YT <larsjedikkemoder@gmail.com>
2021-01-09 12:15:56 +0100

    init

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
-function memory() {
+module.exports = function(){
     const arr = [1, 2, 3, 4, 5, 6, 9, 7, 8, 9, 10];
     arr.reverse();
     const used = process.memoryUsage().heapUsed / 1024 / 1024;
     var mb = Math.round(used * 100) / 100
     return mb;
 }
-
-module.exports.memory = this.memory;
